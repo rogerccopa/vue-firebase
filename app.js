6d75623475d34f41a5b5bf2841e4ba61c3ed0acc
Roger Ccopa <rogerccopa@gmail.com>
2021-02-07 13:47:52 -0500

    Looping with v-for

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ new Vue({
     el:'#app',
     data: {
         title: 'Becoming a Vue ninja',
-        showName: false
+        items: ['Mushroom', 'Green Shell', 'Red Shell', 'Banana', 'Star'],
+        ninjas: [
+            {name:'Roger', age:49, belt:'Black'},
+            {name:'Sonia', age:54, belt:'Brown'},
+            {name:'Croquet', age:24, belt:'Orange'}
+        ]
     },
     methods: {
-        toogleShowName(){
-            this.showName = !this.showName;
-        }
+        
     }
 })
